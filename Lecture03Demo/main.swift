1f08afbd0d3a0065148c58e2f332882e183ef526
Jonathan Engelsma <jengelsma@gmail.com>
2016-05-17 19:48:32 -0400

    Initial Commit

---
+++ Lecture03Demo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lecture03Demo
+//
+//  Created by Jonathan Engelsma on 5/17/16.
+//  Copyright © 2016 Jonathan Engelsma. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
